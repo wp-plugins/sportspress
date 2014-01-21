d4908907eeef73cd10184f14a4fdcb671096568d
ThemeBoy <ThemeBoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-21 09:36:54 +0000

    tagging version 0.1.3
    git-svn-id: https://plugins.svn.wordpress.org/sportspress/trunk@842337 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === SportsPress ===
 Contributors: themeboy
-Tags: sports, sports journalism, teams, team management, fixtures, results, standings, league tables, divisions, reporting, themeboy, wordpress sports, configurable
+Tags: sports, sports journalism, teams, team management, fixtures, results, standings, league tables, leagues, reporting, themeboy, wordpress sports, configurable
 Requires at least: 3.5
-Tested up to: 3.6
-Stable tag: 0.1.2
+Tested up to: 3.8
+Stable tag: 0.1.3
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -11,15 +11,26 @@ SportsPress is a flexible sports management plugin that adds team management fun
 
 == Changelog ==
 
-= 0.1.2 - 12/09/2013 =
+= 0.1.3 =
+* Fix - Style conflict with Foundation framework in table columns.
+* Feature - HTML output added to custom post types with data table sorting.
+* Feature - Enable metrics and statistics per player position.
+* Feature - Save team played per season per league in player edit screen.
+* Feature - Give teams the option to show and hide past seasons.
+* Feature - Venues, Seasons, and Leagues added as taxonomies.
+* Feature - Add L10 counter to report last 10 outcomes.
+* Feature - Add STRK counter to report current outcome streak.
+* Localization - Add country names in Czech, German, Spanish, French, Italian, Japanese, Polish, Russian, and Slovak.
+
+= 0.1.2 =
 * Tweak - Use custom post types for metrics and statistics configuration.
 * Feature - Display HTML tables when viewing League Table and Player List post types.
 * Feature - Link player and team names to single post pages.
 * Feature - Register default configuration for soccer.
 * Feature - Sort league table and player list by priority.
 
-= 0.1.1 - 11/24/2013 =
+= 0.1.1 =
 * Tweak - Update description.
 
-= 0.1 - 11/23/2013 =
+= 0.1 =
 * Alpha release for first look and testing.
--- sportspress.php
+++ sportspress.php
@@ -6,9 +6,9 @@
 Plugin Name: SportsPress
 Plugin URI: http://themeboy.com/sportspress
 Description: Manage your club and its players, staff, events, league tables, and player lists.
-Version: 0.1
+Version: 0.1.3
 Author: ThemeBoy
-Author URI: http://themeboy.com
+Author URI: http://themeboy.com/
 License: GPLv3
 */
 
@@ -18,27 +18,48 @@ if ( !function_exists( 'add_action' ) ) {
 	exit;
 }
 
-define( 'SPORTSPRESS_VERSION', '0.1' );
+define( 'SPORTSPRESS_VERSION', '0.1.3' );
 define( 'SPORTSPRESS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'SPORTSPRESS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define( 'SPORTSPRESS_PLUGIN_FILE', __FILE__ );
 
 // Libraries
-include dirname( __FILE__ ) . '/lib/classes/eos.class.php' ;
+include dirname( __FILE__ ) . '/lib/eos/eos.class.php' ;
 
 // Globals
-include dirname( __FILE__ ) . '/sportspress-globals.php' ;
+include_once dirname( __FILE__ ) . '/admin/globals/continents.php';
+include_once dirname( __FILE__ ) . '/admin/globals/countries.php';
+include_once dirname( __FILE__ ) . '/admin/globals/sports.php';
 
 // Functions
-require_once dirname( __FILE__ ) . '/sportspress-functions.php';
+require_once dirname( __FILE__ ) . '/functions.php';
+
+// Templates
+require_once dirname( __FILE__ ) . '/admin/templates/event-details.php';
+require_once dirname( __FILE__ ) . '/admin/templates/event-players.php';
+require_once dirname( __FILE__ ) . '/admin/templates/event-results.php';
+require_once dirname( __FILE__ ) . '/admin/templates/event-staff.php';
+require_once dirname( __FILE__ ) . '/admin/templates/event-venue.php';
+require_once dirname( __FILE__ ) . '/admin/templates/events-calendar.php';
+require_once dirname( __FILE__ ) . '/admin/templates/league-table.php';
+require_once dirname( __FILE__ ) . '/admin/templates/player-league-statistics.php';
+require_once dirname( __FILE__ ) . '/admin/templates/player-list.php';
+require_once dirname( __FILE__ ) . '/admin/templates/player-metrics.php';
+require_once dirname( __FILE__ ) . '/admin/templates/player-statistics.php';
+require_once dirname( __FILE__ ) . '/admin/templates/team-columns.php';
 
 // Settings
-include dirname( __FILE__ ) . '/sportspress-settings.php' ;
+include dirname( __FILE__ ) . '/admin/settings/settings.php' ;
 
-// Custom Post Types
-require_once dirname( __FILE__ ) . '/admin/post-types/event.php';
+// Custom post types
+require_once dirname( __FILE__ ) . '/admin/post-types/separator.php';
+require_once dirname( __FILE__ ) . '/admin/post-types/column.php';
+require_once dirname( __FILE__ ) . '/admin/post-types/statistic.php';
+require_once dirname( __FILE__ ) . '/admin/post-types/metric.php';
 require_once dirname( __FILE__ ) . '/admin/post-types/result.php';
 require_once dirname( __FILE__ ) . '/admin/post-types/outcome.php';
-require_once dirname( __FILE__ ) . '/admin/post-types/stat.php';
-require_once dirname( __FILE__ ) . '/admin/post-types/metric.php';
+require_once dirname( __FILE__ ) . '/admin/post-types/event.php';
+require_once dirname( __FILE__ ) . '/admin/post-types/calendar.php';
 require_once dirname( __FILE__ ) . '/admin/post-types/team.php';
 require_once dirname( __FILE__ ) . '/admin/post-types/table.php';
 require_once dirname( __FILE__ ) . '/admin/post-types/player.php';
@@ -46,31 +67,37 @@ require_once dirname( __FILE__ ) . '/admin/post-types/list.php';
 require_once dirname( __FILE__ ) . '/admin/post-types/staff.php';
 
 // Terms
-require_once dirname( __FILE__ ) . '/admin/terms/div.php';
-require_once dirname( __FILE__ ) . '/admin/terms/pos.php';
+require_once dirname( __FILE__ ) . '/admin/terms/league.php';
+require_once dirname( __FILE__ ) . '/admin/terms/season.php';
+require_once dirname( __FILE__ ) . '/admin/terms/venue.php';
+require_once dirname( __FILE__ ) . '/admin/terms/position.php';
 
-// Shortcodes
-// require_once dirname( __FILE__ ) . '/classes/shortcodes/table.php';
+// Typical request actions
+require_once dirname( __FILE__ ) . '/admin/hooks/plugins-loaded.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/after-setup-theme.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/wp-enqueue-scripts.php';
 
-// Defaults
-include dirname( __FILE__ ) . '/sportspress-defaults.php';
+// Admin request actions
+require_once dirname( __FILE__ ) . '/admin/hooks/admin-menu.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/admin-enqueue-scripts.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/admin-print-styles.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/admin-head.php';
 
-// Hooks, Actions, and Filters
-require_once dirname( __FILE__ ) . '/sportspress-hooks.php';
-require_once dirname( __FILE__ ) . '/sportspress-actions.php';
-require_once dirname( __FILE__ ) . '/sportspress-filters.php';
+// Administrative actions
+require_once dirname( __FILE__ ) . '/admin/hooks/manage-posts-custom-column.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/post-thumbnail-html.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/restrict-manage-posts.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/save-post.php';
 
-// Admin Styles
-function sp_admin_styles() {
-	wp_register_style( 'sportspress-admin', SPORTSPRESS_PLUGIN_URL . 'assets/css/admin.css', array(), '1.0' );
-	wp_enqueue_style( 'sportspress-admin');
-}
-add_action( 'admin_init', 'sp_admin_styles' );
+// Filters
+require_once dirname( __FILE__ ) . '/admin/hooks/admin-post-thumbnail-html.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/gettext.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/pre-get-posts.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/sanitize-title.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/the-content.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/wp-insert-post-data.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/plugin-action-links.php';
+require_once dirname( __FILE__ ) . '/admin/hooks/post-updated-messages.php';
 
-// Admin Scripts
-function sp_admin_enqueue_scripts() {
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'sportspress-admin', SPORTSPRESS_PLUGIN_URL .'/assets/js/admin.js', array( 'jquery' ), time(), true );
-}
-add_action( 'admin_enqueue_scripts', 'sp_admin_enqueue_scripts' );
-?>
+// Register activation hook
+require_once dirname( __FILE__ ) . '/admin/hooks/register-activation-hook.php';
